b43dc7147c48a7d1ebb4f72f63b22c86cf16c54b
jcenglish <contact@jasmineenglish.com>
2018-03-08 18:38:27 -0500

    Made multiplication function less resource intensive... however, now it's stuck in a loop. It's probably the getRandomFactor function.
    Added a function to make fractions look pretty, but fractions are still
    ugly.
    
    Tidied up the code a bit.
    
    Tests use innerHTML instead of innerText now.

--- app.js
+++ app.js
@@ -7,10 +7,8 @@
 // I would have loved something like this for my students!
 // Eventually I will build it with a Raspberry Pi setup
 // and rewrite the code in something else, like Python
-
 // TODO: A proper LCD font or SVG alternative. This font's
 // division and plus signs look too similar
-
 // Yikes, where do I start... an array 60 elements long where
 // each element is an array of answers for that index
 // (which represents a minute or hour)?
@@ -18,7 +16,11 @@
 // It just doesn't seem very practical, or at least,
 // not very elegant.
 
-var ops = {
+//-----------------------------------------------------------------------
+// CONSTANTS AND VARIABLES
+//-----------------------------------------------------------------------
+
+const OPS = {
     fraction: "/",
     power: "^",
     minus: "−",
@@ -28,131 +30,133 @@ var ops = {
     root: "√"
 };
 
-var primes = [
-  1,
-  2,
-  3,
-  5,
-  7,
-  11,
-  13,
-  17,
-  19,
-  23,
-  29,
-  31,
-  37,
-  41,
-  43,
-  47,
-  53,
-  59
-];
+const PRIMES = [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59 ];
+
+//var difficulty;
+
+//-----------------------------------------------------------------------
+// UTILITY FUNCTIONS
+//-----------------------------------------------------------------------
+
+//FIX: make the fraction readable - looks wonky right now
+function formatFraction(numerator, denominator){
+  return numerator.toString().sup() + OPS.fraction + denominator.toString().sub();
+}
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min)) + min;
+ //The maximum is exclusive and the minimum is inclusive
 }
 
-function getRandomMultiple(max, factor){
-  var random = Math.random() * (max - factor + 1) + factor;
-  return random - (random % factor);
+function getRandomMultiple(max, factor) {
+    var random = Math.random() * (max - factor + 1) + factor;
+    return random - random % factor;
 }
 
-//FIX: only get random number within set of multiples of answer? to lower resource usage?
-function getDivisionProblem(answer){
-  console.log('--------answer: ' + answer);
+//FIX: Should probably save prime factors for each number, 2-59. Doesn't
+// make sense to recalculate them each time
+function getRandomFactor(number){
+  //get prime factors
+  var pFactors = [];
+  var product = 1
+  var i = 0;
+  var currentNumber = number;
 
-if (answer == 0){
-      return 0 + ops.divide + getRandomInt(1,60);
+  while (product != number){
+    if (number % PRIMES[i] == 0){
+      pFactors.push(PRIMES[i]);
+      currentNumber = number / PRIMES[i];
+      product *= PRIMES[i];
+      i = 0;
     }
+    i++;
+  }
 
+  //To get random factor, multiply 2 or more prime factors together
+  var numPFactors = getRandomInt(1, pFactors.length);
+  var randomFactor = 1;
+  var currentPrimeIndex = '';
+  for (var j = 0; j <= numPFactors; j++){
+    currentPrimeIndex = getRandomInt(0, pFactors.length - 1);
+    randomFactor *= pFactors[currentPrimeIndex];
+    pFactors.splice(currentPrimeIndex, 1);
+  }
+
+  return randomFactor;
+}
+
+//-----------------------------------------------------------------------
+// MATH PROBLEM GENERATORS
+//-----------------------------------------------------------------------
+
+function getDivisionProblem(answer) {
+    console.log("--------answer: " + answer);
+    if (answer == 0) {
+        return 0 + OPS.divide + getRandomInt(1, 60);
+    }
     var b = getRandomMultiple(60, answer);
     var a = b / answer;
-    //var problem = '';
 
-    
-//while (b % a != 0) {
-// if (b / a == answer) {
-//             console.log('found answer');
-//             problem = b + ops.divide + a;
-//             break; // FIX: Are breaks the only way to avoid undefined returns?
-//         }
-//             console.log('not a divisor: ' + a);
-//             b = getRandomInt(answer*2, 60);
-// }
-
-    return b + ops.divide + a; 
+    return b + OPS.divide + a;
 }
 
-
-// FIX: There's probably a less resource-intensive way to do this
-// Get a random int from an array of 60 or 24 numbers and remove that int if it
-// doesn't satisfy the function?
 function getMultiplicationProblem(answer) {
-
-    console.log('--------answer: ' + answer);
-
-    var a = 2;
-    var b = getRandomInt(2, Math.ceil(answer / 2));
-    var problem = '';
-    if (primes.includes(answer)) {
+    console.log("--------answer: " + answer);
+    if (PRIMES.includes(answer)) {
         console.log("it's a prime");
-        return 1 + ops["times"] + answer;
+        return 1 + OPS.times + answer;
     } else if (answer == 0) {
-        return 0 + ops.times + getRandomInt(0, 60);
-    }
-    while (a <= Math.floor(answer / 2)) {
-        console.log('while runs');
-        console.log('b: ' + b);
+        return 0 + OPS.times + getRandomInt(0,60);
+      }
+      else if (answer == 1){
+        var denominator = getRandomInt(0,60);
+        return denominator + OPS.times + formatFraction(1, denominator);
+      }
 
-        if (a * b == answer) {
-            console.log('found answer');
-            problem = a + ops.times + b;
-            break; // FIX: Are breaks the only way to avoid undefined returns?
-        }
-        if (a == Math.floor(answer / 2)) {
-            console.log('a reached end: ' + a);
-            a = 2;
-            b = getRandomInt(2, Math.ceil(answer / 2));
-        }
-        console.log('a: ' + a);
-        a++;
-    }
-    return problem;
+    var b = getRandomFactor(answer);
+    var a = answer / b;
+
+    console.log("b: " + b);
+    console.log("a: " + a);
+    return b + OPS.times + a;
 }
+
+//-----------------------------------------------------------------------
+// CLOCK FUNCTIONS, CONSTANTS AND VARIABLES
+//-----------------------------------------------------------------------
 
 function startClock() {
     var time = new Date();
     // The actual time
     document.getElementById("problem-hr").innerHTML = time.getHours();
     document.getElementById("problem-min").innerHTML = time.getMinutes();
-
     setTimeout(startClock, 500);
 }
 
 //startClock();
 
-// Tests -------------------------------------------------------------------------------------
-
-function testMultiplication(){
-  for (var i = 0; i < 60; i++) {
-    console.log('---------------outer loop');
-    var li = document.createElement("li");
-    li.innerText = '[' + i + '] ' + getMultiplicationProblem(i);
-    document.getElementById("multiplication").appendChild(li);
-}
-}
-
-function testDivision(){
-  for (var i = 0; i < 10; i++) {
-    console.log('---------------outer loop');
-    var li = document.createElement("li");
-    li.innerText = '[' + i + '] ' + getDivisionProblem(i);
-    document.getElementById("division").appendChild(li);
-}
+//-----------------------------------------------------------------------
+// TESTS
+//-----------------------------------------------------------------------
+function testMultiplication() {
+    for (var i = 0; i < 10; i++) {
+        console.log("---------------outer loop");
+        var li = document.createElement("li");
+        li.innerHTML = "[" + i + "] " + getMultiplicationProblem(i);
+        document.getElementById("multiplication").appendChild(li);
+    }
 }
 
-testDivision();
-//testMultiplication();
+function testDivision() {
+    for (var i = 0; i < 10; i++) {
+        console.log("---------------outer loop");
+        var li = document.createElement("li");
+        li.innerHTML = "[" + i + "] " + getDivisionProblem(i);
+        document.getElementById("division").appendChild(li);
+    }
+}
+
+//testDivision();
+testMultiplication();
